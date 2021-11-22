0d4c0a80302c260fdcc76c9afb8d96bda3d09ec4
tcowie1 <89547858+tcowie1@users.noreply.github.com>
2021-11-22 15:09:55 -0500

    Update streamlit_app.py

--- Deployed/streamlit-example-master/streamlit_app.py
+++ Deployed/streamlit-example-master/streamlit_app.py
@@ -4,8 +4,7 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# About
+# Home
 
 At UniSight, we want to provide accessible, easy-to-read data and 
 visuals to help potential and current students make informed decisions 
@@ -13,9 +12,9 @@ on college degrees and the careers they lead to. Guiding individuals through
 major college and career decisions, with up-to-date information, is our 
 objective.
 
-# Select a hoe
+# Navigation
 """
-
+navChoice = st.selectbox('Navigation', ('Home', 'Salaries', 'Loans'))
 total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
